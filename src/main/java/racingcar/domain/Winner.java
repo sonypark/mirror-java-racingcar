6f1d19bda740293ec831b5fede4d1bf4958eb996
Sony <sonypark0204@gmail.com>
2020-02-17 10:37:17 +0900

    [refactor] 변수명에 자료구조 이름(List) 제외

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class Winner {
     private final List<String> winners = new ArrayList<>();
 
-    public List<String> findWinners(List<Car> carList) {
-        int max = findWinnerPosition(carList);
+    public List<String> findWinners(List<Car> cars) {
+        int max = findWinnerPosition(cars);
 
-        carList.stream()
+        cars.stream()
             .filter(car -> car.isWinner(max))
             .forEach(car -> winners.add(car.getName()));
 
